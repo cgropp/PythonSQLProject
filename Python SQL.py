7e66a72a117e54864325e84157f9e2bbee0dc7dd
Cameron Gropp <cgropp@ucsd.edu>
2021-07-19 18:20:39 -0700

    Successful database connection

--- Python SQL.py
+++ Python SQL.py
@@ -1,1 +1,19 @@
-#Interaction between python and SQL
+import mysql.connector
+
+db = mysql.connector.connect(host="localhost",    
+							user="PythonDBUser",         
+							passwd="PythonDBPass",  
+							db="invoicing")
+
+
+testSelectQuery = "SELECT * FROM clients WHERE client_id > 1;"
+
+with db.cursor() as cursor:
+	cursor.execute(testSelectQuery)
+	result = cursor.fetchall()
+	for row in result:
+		print(row)
+	cursor.close()
+
+
+db.close()
